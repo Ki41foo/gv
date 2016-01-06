16a8826ccc263fe94cce0d63adff87ca13ed524e
ki41foo <ki41foo@gmail.com>
2016-01-06 13:48:22 +0000

    add bar chart for restaurant lottery count

--- app/controllers/lottery_record_controller.rb
+++ app/controllers/lottery_record_controller.rb
@@ -5,11 +5,18 @@ class LotteryRecordController < ApplicationController
     def index
        
         
+        
         @total_count = LotteryRecord.count
         @lottery_records = LotteryRecord.where("result=1").reverse_order
         @bingo_count = @lottery_records.count
         @count_data = LotteryRecord.group(:timestamp).count
         @signature_data = LotteryRecord.group(:signature).count
+        
+        hash = LotteryRecord.group(:ti).count
+        hash.keys.each {
+            |k| hash[LotteryRecordHelper::Mers[k]] = hash.delete(k)
+        }
+        @detail = hash
   end
   
   
